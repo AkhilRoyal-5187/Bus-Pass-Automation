727bd0fe7a1d34f73a3e060e9f09a0a9bb18fef3
Guna Shekar <gunashekar@Gunas-MacBook-Pro.local>
2025-05-16 21:59:39 +0530

    added dash board pages

---
+++ src/app/admindash/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+
+export default function AdminDash () {
+    return(
+        console.log("admin dashboard"),
+        <div className="flex flex-col items-center c h-screen bg-gray-100 text-blue-400">
+            <p>Bus pass requests</p>
+
+        
+
+        </div>
+    )
+}
---
+++ src/app/studentdash/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from "react";
+
+export default function  StudentDash(){
+    return(
+        console.log("admin dashboard"),
+        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+            <h1 className="text-4xl font-bold mb-4 text-amber-300">student Dashboard</h1>
+            <p className="text-lg mb-2">Welcome to the student Dashboard!</p>
+
+
+        
+
+        </div>
+    )
+}
